705df1d73981160cd52eb23f0547cfbae6608292
Aleksandr <aleksandrfilippov2003@mail.ru>
2025-09-03 18:09:52 +0300

    add auth router

--- app/api/routers/__init__.py
+++ app/api/routers/__init__.py
@@ -1,6 +1,7 @@
+from .auth import router as auth
 from .products import router as products
 from .locations import router as locations
 from .inventory import router as inventory
-from .purchase_orders import router as purchase
+from .purchase_orders import router as purchase_orders
 
-__all__ = ["products", "locations", "inventory", "purchase"]
+__all__ = ["auth", "products", "locations", "inventory", "purchase_orders"]
